79e1f0714a3ed7ba8bb96fdc87c447425e17fbf9
Felipe Calderon Barragan <felipeskabarragan@gmail.com>
2013-05-06 13:38:21 -0500

    Initial Commit

---
+++ Agbo2/FCBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FCBAppDelegate.h
+//  Agbo2
+//
+//  Created by WTFCALDERON on 6/05/13.
+//  Copyright (c) 2013 WideTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
